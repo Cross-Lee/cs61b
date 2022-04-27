157b1211ee9b50599e35173a23d4fd66c69f7b2c
Lee <963639875@qq.com>
2022-04-27 10:08:43 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args){
+        int x = 0;
+        int i = 1;
+        while (x < 46){
+            System.out.print(x + "10" + "a");
+            x += i;
+            i += 1;
+        }
+    }
+}
